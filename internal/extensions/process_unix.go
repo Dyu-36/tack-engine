f1b1311170d20982b81424cac1119710c033961d
Dyu-36 <nguyenvanducduy2006@gmail.com>
2026-09-21 00:41:41 +0700

    feat: add Go-native external tool extensions

---
+++ internal/extensions/process_unix.go
@@ -0,0 +1,23 @@
+//go:build !windows
+
+package extensions
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func configureCommand(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+}
+
+func killProcessTree(cmd *exec.Cmd) error {
+	if cmd == nil || cmd.Process == nil {
+		return nil
+	}
+	err := syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+	if err == syscall.ESRCH {
+		return nil
+	}
+	return err
+}
